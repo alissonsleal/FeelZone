aabae4a357f06615d44e40b4008c19fc45de93d1
Alisson <alissonsleal@hotmail.com>
2020-07-13 18:52:59 -0300

    bug fix: keys are now numbers;

--- screens/home.js
+++ screens/home.js
@@ -5,14 +5,19 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Home({ navigation }) {
   const [review, setReview] = useState([
-    { title: "Mario", rating: 5, body: "lorem ipsum", key: 1 },
+    { title: "Mario", rating: "5", body: "lorem ipsum", key: "1" },
     {
       title: "Gotta catch Them All(again)",
-      rating: 4,
+      rating: "4",
       body: "lorem ipsum",
-      key: 2,
+      key: "2",
     },
-    { title: 'Not So "Final" Fantasy', rating: 3, body: "lorem ipsum", key: 3 },
+    {
+      title: 'Not So "Final" Fantasy',
+      rating: "3",
+      body: "lorem ipsum",
+      key: "3",
+    },
   ]);
 
   return (
