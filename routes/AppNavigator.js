eb0cd5ba3d5789714dbf15a66c8c1b4dbd4b0fb9
Alisson <alissonsleal@hotmail.com>
2020-07-13 19:13:09 -0300

    imported drawerNav

--- routes/AppNavigator.js
+++ routes/AppNavigator.js
@@ -1,20 +1,26 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../screens/home";
 import ReviewDetails from "../screens/reviewDetails";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={{ title: "" }} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "GameZone" }}
+        />
         <Stack.Screen
           name="ReviewDetails"
           component={ReviewDetails}
-          options={{ title: "" }}
+          options={({ route }) => ({ title: route.params.title })}
         />
       </Stack.Navigator>
     </NavigationContainer>
