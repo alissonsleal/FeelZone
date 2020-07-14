4c71d028411936ed53352bd381630d8a025954e6
Alisson <alissonsleal@hotmail.com>
2020-07-13 22:42:41 -0300

    removed some unnecessary about text

--- screens/about.js
+++ screens/about.js
@@ -5,7 +5,6 @@ import { globalStyles } from "../styles/global";
 export default function About({ navigation }) {
   return (
     <View style={globalStyles.container}>
-      <Text>About</Text>
       <Button title="fuc go bac" onPress={() => navigation.navigate("Home")} />
     </View>
   );
