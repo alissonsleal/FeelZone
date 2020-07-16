1b39e45700dd4c553f13536fbb6158ea3595bf69
Alisson <alissonsleal@hotmail.com>
2020-07-16 18:13:54 -0300

    form validation working

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,12 +3,25 @@ import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
 import Card from "../shared/card";
+import * as yup from "yup";
+
+const reviewSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("numIs1-5", "Rating must be a number between 1 and 5", (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
 export default ReviewForm = ({ addReview }) => {
   return (
     <View styles={globalStyles.container}>
       <Formik
         initialValues={{ title: "", body: "", rating: "" }}
+        validationSchema={reviewSchema}
         onSubmit={(values, actions) => {
           actions.resetForm();
           addReview(values);
