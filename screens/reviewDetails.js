7ca42213e556b2b4cc228f763fd2a1abc7a9abd0
Alisson <alissonsleal@hotmail.com>
2020-08-10 19:43:40 -0300

    removed frontend for the rating system

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -10,10 +10,12 @@ export default function ReviewDetails({ route, navigation }) {
     <View style={globalStyles.container}>
       <Card>
         <Text style={globalStyles.titleText}>{title}</Text>
-        <Text style={globalStyles.paragraph}>{body}</Text>
+        <Text style={[globalStyles.paragraph, styles.spaceCard]}>{body}</Text>
+        {/* ****** Code for a "Rating of the day/vent" ********
         <View>
           <Text style={styles.ratingText}>Rating: {rating} / 5</Text>
         </View>
+        */}
         <Button
           title="Go back"
           onPress={() => navigation.navigate("Home")}
@@ -25,10 +27,16 @@ export default function ReviewDetails({ route, navigation }) {
   );
 }
 const styles = StyleSheet.create({
+  spaceCard: {
+    paddingBottom: 20,
+  },
+
+  /*  ******** Code for a "Rating of the day/vent" ********
   ratingText: {
     color: globalStyles.light.color,
     paddingVertical: 20,
     fontSize: 16,
     letterSpacing: 1,
   },
+  */
 });
